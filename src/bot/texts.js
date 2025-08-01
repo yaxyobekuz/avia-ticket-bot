b4182fc58a28479f0d66030b2fea424d5d5edd50
yaxyobekuz <pubgn9642@gmail.com>
2025-08-01 15:34:07 +0500

    Add ticket info texts

--- src/bot/texts.js
+++ src/bot/texts.js
@@ -1,14 +1,25 @@
 const texts = {
-  // Confirmation
-  done: "Tayyor ✅",
-  confirm: `✅ Tasdiqlash`,
-  confirmAndSave: `✅ Tasdiqlash va Saqlash`,
-  ticketSaved: `✅ Chipta muvaffaqiyatli saqlandi!`,
+  home: `Bosh menyuga qaytdingiz!`,
+
+  // Ticket
+  ticketInfo: `Chipta ma'lumotlari 📄`,
+  ticketNotFound: `Chipta topilmadi! 🤷‍♂️`,
+  ticketFiles: `Chipta fayllari \(\ma'lumotlar bilan) 📂`,
+  ticketFilesWithoutInfo: `Chipta fayllari (ma'lumotlarsiz) 📁`,
+  selectFromMenu: `Quyidagi menyuda o'zingizga keraklisini tanlang: 👇`,
 
   // Cancel
   cancel: `❌ Bekor Qilish`,
   operationCancelled: `❌ Operatsiya bekor qilindi.`,
   cancelAndRestart: `❌ Bekor Qilish va Qaytadan Boshlash`,
+  ticketSavedErrorInSheets: `❌ Chipta Google sheetsga saqlanmadi!`,
+
+  // Confirmation
+  done: "Tayyor ✅",
+  confirm: `✅ Tasdiqlash`,
+  confirmAndSave: `✅ Tasdiqlash va Saqlash`,
+  ticketSavedToSheets: `✅ Chipta muvaffaqiyatli Google sheetsga saqlandi!`,
+  ticketSaved: `✅ Chipta muvaffaqiyatli saqlandi va Google sheetsga saqlanmoqda...`,
 
   // Booking
   addNewTicket: `📝 Yangi Chipta Qo'shish`,
@@ -22,6 +33,7 @@ const texts = {
   invalidPrice: `❌ Iltimos, klaviaturadan to'g'ri narxni tanlang.`,
   invalidData: `❌ Iltimos, mijoz ma'lumotlarini to'g'ri kiriting.`,
   invalidDirection: `❌ Iltimos, klaviaturadan to'g'ri yo'nalishni tanlang.`,
+  invalidKeyboard: `❌ Iltimos, menyuda berilgan variantlardan birini tanlang.`,
   uploadAtLeastOneDocument: `❌ Davom etishdan oldin kamida bitta hujjat yuklang.`,
   documentUploadError: `❌ Hujjatni yuklashda xatolik. Iltimos, qaytadan urinib ko'ring.`,
   get sendDocuments() {
