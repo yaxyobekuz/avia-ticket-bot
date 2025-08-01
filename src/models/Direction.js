42c53dd79d15035f3666b7759aa71d08ac6a15e5
yaxyobekuz <pubgn9642@gmail.com>
2025-08-01 16:21:36 +0500

    Delete unused models

--- src/models/Direction.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const directionSchema = new mongoose.Schema({
-  name: { type: String, default: "Noma'lum" },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Direction", directionSchema);
--- src/models/Price.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const priceSchema = new mongoose.Schema({
-  value: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Price", priceSchema);
